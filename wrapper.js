1eac6e470efaca0e3e5613c86a2a5db2eb70c0e4
Anton Yurchenko <anton.doar@gmail.com>
2020-06-27 02:00:57 +0300

    chore(wrapper): warn only on wrong configuration

--- wrapper.js
+++ wrapper.js
@@ -3,7 +3,6 @@ const os = require('os');
 
 function execute(binary, system) {
     core.info(`[wrapper] runner platform: '${system}'`);
-    core.info(`[wrapper] arguments: '${core.getInput('args')}'`);
 
     require('child_process').execFileSync(
         binary,
@@ -12,18 +11,15 @@ function execute(binary, system) {
     );
 }
 
-// notify that there is a better way of execution :-)
-core.warning(`[wrapper] executing this action via wrapper is not recommended! see README for more information`);
-
-// validate cpu architecture
 if (os.arch != `x64`) {
     core.setFailed(`[wrapper] runner cpu architecture is not supported: '${os.arch}'`);
 }
 
-// execute correct binary basing on operation system
 if (os.type == `Windows_NT`) {
     execute(`${__dirname}\\build\\git-release-windows-amd64.exe`, os.type);
 } else if (os.type == `Linux`) {
+    core.warning(`Executing this action via wrapper is not recommended on Linux runner!`);
+
     execute(`${__dirname}/build/git-release-linux-amd64`, os.type);
 } else {
     core.setFailed(`[wrapper] runner operation system is not supported: '${os.type}'`);
