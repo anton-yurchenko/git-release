826cc90ace90308b8735a690b1d5aa1b88b0effa
Anton Yurchenko <anton.doar@gmail.com>
2020-07-01 23:49:54 +0300

    chore(wrapper): create main func

--- wrapper.js
+++ wrapper.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const os = require('os');
+const path = require('path');
 
 function execute(binary, system) {
     core.info(`[wrapper] runner platform: '${system}'`);
@@ -11,16 +12,24 @@ function execute(binary, system) {
     );
 }
 
-if (os.arch != `x64`) {
-    core.setFailed(`[wrapper] runner cpu architecture is not supported: '${os.arch}'`);
+function main() {
+    if (os.arch != `x64`) {
+        core.setFailed(`[wrapper] runner cpu architecture is not supported: '${os.arch}'`);
+        process.exit(1);
+    }
+
+    if (process.platform == `win32`) {
+        filename = `git-release-windows-amd64.exe`
+    } else if (process.platform == `linux`) {
+        core.warning(`Executing this action via wrapper is not recommended on Linux runner!`);
+        filename = `git-release-linux-amd64`
+    } else {
+        core.setFailed(`[wrapper] runner operation system is not supported: '${process.platform}'`);
+        process.exit(1);
+    }
+
+    file = path.join(__dirname, `build/${filename}`)
+    execute(file, process.platform);
 }
 
-if (os.type == `Windows_NT`) {
-    execute(`${__dirname}\\build\\git-release-windows-amd64.exe`, os.type);
-} else if (os.type == `Linux`) {
-    core.warning(`Executing this action via wrapper is not recommended on Linux runner!`);
-
-    execute(`${__dirname}/build/git-release-linux-amd64`, os.type);
-} else {
-    core.setFailed(`[wrapper] runner operation system is not supported: '${os.type}'`);
-}
+main()
