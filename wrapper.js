95ba60b15785dbe7c64766e0a4ee3746e52d14ea
Anton Yurchenko <anton.doar@gmail.com>
2020-07-10 22:29:35 +0300

    refactor: wrapper

--- wrapper.js
+++ wrapper.js
@@ -1,35 +1,41 @@
-const core = require('@actions/core');
-const os = require('os');
-const path = require('path');
+const core = require('@actions/core')
+const os = require('os')
+const path = require('path')
+const fs = require('fs')
+const child = require('child_process')
 
-function execute(binary, system) {
-    core.info(`[wrapper] runner platform: '${system}'`);
-
-    require('child_process').execFileSync(
-        binary,
-        [core.getInput('args')],
-        { stdio: 'inherit' }
-    );
+function execute (file) {
+  if (fs.existsSync(file)) {
+    child.execFileSync(
+      file,
+      [core.getInput('args')],
+      { stdio: 'inherit' }
+    )
+  } else {
+    core.setFailed(`[wrapper] file not found: '${file}'`)
+    process.exit(1)
+  }
 }
 
-function main() {
-    if (os.arch != `x64`) {
-        core.setFailed(`[wrapper] runner cpu architecture is not supported: '${os.arch}'`);
-        process.exit(1);
-    }
+function main () {
+  if (os.arch() !== 'x64') {
+    core.setFailed(`[wrapper] runner cpu architecture is not supported: '${os.arch}'`)
+    process.exit(1)
+  }
 
-    if (process.platform == `win32`) {
-        filename = `git-release-windows-amd64.exe`
-    } else if (process.platform == `linux`) {
-        core.warning(`Executing this action via wrapper is not recommended on Linux runner!`);
-        filename = `git-release-linux-amd64`
-    } else {
-        core.setFailed(`[wrapper] runner operation system is not supported: '${process.platform}'`);
-        process.exit(1);
-    }
+  let filename
 
-    file = path.join(__dirname, `build/${filename}`)
-    execute(file, process.platform);
+  if (process.platform === 'win32') {
+    filename = 'git-release-windows-amd64.exe'
+  } else if (process.platform === 'linux') {
+    core.warning('Executing this action via wrapper is not recommended on Linux runner!')
+    filename = 'git-release-linux-amd64'
+  } else {
+    core.setFailed(`[wrapper] runner operation system is not supported: '${process.platform}'`)
+    process.exit(1)
+  }
+
+  execute(path.join(__dirname, 'build', filename))
 }
 
 main()
